67ae1c47969fccfdbb55085ea8db0f4cc5bf5887
Gustavo <gus@terpmail.umd.edu>
2020-06-16 04:52:03 -0400

    Include seasonality_test

--- R/seasonality_test.R
+++ R/seasonality_test.R
@@ -1,11 +1,41 @@
-# > Seasonality Test ###################################################### ----
+# >  ###################################################### ----
 
 # Used to determine whether a time series is seasonal.
 # Code modified from the M4 Competition's original code.
 
-seasonality_test <- function(ts.data, ts.freq, alpha.level){
+#' Seasonality Test
+#'
+#' Used to determine whether a time series is seasonal. Code modified from the
+#' M4 Competition's original code.
+#'
+#' @param ts.data A ts object (i.e., a vector of time series observations).
+#' @param ts.freq The frequency of a ts object.
+#' @param alpha.level The alpha level to be used in the test to detect
+#'     seasonality. Default is 0.05.
+#'
+#' @return A boolean indicating whether or not the ts object has seasonality.
+#'
+#' @examples
+#' seasonality_test(
+#'   ts.data = datasets::WWWusage,
+#'   ts.freq = frequency(datasets::WWWusage),
+#'   alpha.level = 0.05
+#' )
+#'
+#' seasonality_test(
+#'   ts.data = datasets::AirPassengers,
+#'   ts.freq = frequency(datasets::AirPassengers),
+#'   alpha.level = 0.05
+#' )
+#'
+#' seasonality_test(
+#'   ts.data = datasets::EuStockMarkets[, "DAX"],
+#'   ts.freq = frequency(datasets::EuStockMarkets[, "DAX"]),
+#'   alpha.level = 0.05
+#' )
+seasonality_test <- function(ts.data, ts.freq, alpha.level = 0.05){
 
-  tmp.tmp.tcrit <- qnorm(1 - alpha.level)
+  tmp.tcrit <- qnorm(1 - alpha.level)
 
   if (length(ts.data) < 3*ts.freq){
 
@@ -28,31 +58,3 @@ seasonality_test <- function(ts.data, ts.freq, alpha.level){
   # |__ RETURN =================================================================
   return(test_seasonal)
 }
-
-# Example
-
-# seasonality_test(
-#   ts.data = insample,
-#   ts.freq = frequency(insample),
-#   alpha.level = 0.05
-# )
-
-seasonality_test(
-  ts.data = datasets::WWWusage,
-  ts.freq = frequency(datasets::WWWusage),
-  alpha.level = 0.05
-)
-
-seasonality_test(
-  ts.data = datasets::AirPassengers,
-  ts.freq = frequency(datasets::AirPassengers),
-  alpha.level = 0.05
-)
-
-seasonality_test(
-  ts.data = datasets::EuStockMarkets[, "DAX"],
-  ts.freq = frequency(datasets::EuStockMarkets[, "DAX"]),
-  alpha.level = 0.05
-)
-
-
