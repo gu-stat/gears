483317a14b80cf5283ad73438750892f716dabb6
Gustavo <gus@terpmail.umd.edu>
2020-06-16 04:51:27 -0400

    Include forecast_naive2

--- R/forecast_naive2.R
+++ R/forecast_naive2.R
@@ -3,7 +3,34 @@
 # Used to return the point forecasts using the Naive2 method.
 # Code modified from the M4 Competition's original code.
 
-forecast_naive2 <- function(ts.data, forecast.horizon, alpha.level){
+#' Title
+#'
+#' @param ts.data A numeric vector of time series observations (a ts object).
+#' @param forecast.horizon A numeric value with the length of the forecast lead.
+#' @param alpha.level A numeric value with the alpha level to be used in the
+#'     test to detect seasonality. Default is 0.05.
+#'
+#' @return A ts object of forecasted values obtained using the Naive2 method.
+#'
+#' @examples
+#' forecast_naive2(
+#' ts.data          = datasets::WWWusage,
+#' forecast.horizon = 10,
+#' alpha.level      = 0.05
+#' )
+#'
+#' forecast_naive2(
+#'   ts.data          = datasets::AirPassengers,
+#'   forecast.horizon = 15,
+#'   alpha.level      = 0.05
+#' )
+#'
+#' forecast_naive2(
+#'   ts.data          = datasets::EuStockMarkets[, "DAX"],
+#'   forecast.horizon = 40,
+#'   alpha.level      = 0.05
+#' )
+forecast_naive2 <- function(ts.data, forecast.horizon, alpha.level = 0.05){
 
   tmp.ts.freq <- frequency(ts.data)
 
@@ -49,15 +76,16 @@ forecast_naive2 <- function(ts.data, forecast.horizon, alpha.level){
 
   last.deseason.ts.data <- tail(deseason.ts.data, 1)
 
-  tmp.time  <- tsp(last.deseason.ts.data)[1]
-  tmp.start <- tmp.time + (1/tmp.ts.freq)
-
   tmp.forecast.horizon <- forecast.horizon - 1
+
+  tmp.time  <- tsp(deseason.ts.data)[2]
+  tmp.start <- tmp.time + (1/tmp.ts.freq)
+  tmp.end   <- tmp.start + (tmp.forecast.horizon * 1 / tmp.ts.freq)
 
   forecast.naive <- ts(
     data      = last.deseason.ts.data,
-    start     =  tmp.start,
-    end       = tmp.start + (tmp.forecast.horizon * 1 / tmp.ts.freq),
+    start     = tmp.start,
+    end       = tmp.end,
     frequency = tmp.ts.freq
   )
 
@@ -68,31 +96,3 @@ forecast_naive2 <- function(ts.data, forecast.horizon, alpha.level){
   # |__ RETURN =================================================================
   return(forecast.naive2)
 }
-
-# Examples
-
-# forecast_naive2(
-#   ts.data          = insample,
-#   forecast.horizon = 6,
-#   alpha.level      = 0.05
-# )
-
-forecast_naive2(
-  ts.data          = datasets::WWWusage,
-  forecast.horizon = 10,
-  alpha.level      = 0.05
-)
-
-forecast_naive2(
-  ts.data          = datasets::AirPassengers,
-  forecast.horizon = 10,
-  alpha.level      = 0.05
-)
-
-forecast_naive2(
-  ts.data          = datasets::EuStockMarkets[, "DAX"],
-  forecast.horizon = 10,
-  alpha.level      = 0.05
-)
-
-
