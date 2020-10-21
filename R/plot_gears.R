d125fdca1481f1679423d5807017f9b1b2e70017
gu-stat <gus@terpmail.umd.edu>
2020-10-21 06:13:34 -0400

    Working plot_gears

--- R/plot_gears.R
+++ R/plot_gears.R
@@ -5,120 +5,226 @@
 #' @param fcol Color for the line with forecasts. Default is 4 ("blue").
 #' @param ylim Limits for the Y-axis.
 #' @param main Main title for the plot.
+#' @param sub Sub-title for the plot.
 #' @param xlab Label for the X-axis.
 #' @param ylab Label for the Y-axis.
 #' @param type Line type for the line with observed values. See
 #'     \link[graphics]{plot} for more options.
 #' @param flty Line type for the line with forecasts.
 #' @param flwd Width of the line with forecasts.
+#' @param cex  Size of the main title when \code{betas.selection = "both"}.
 #' @param ... Other arguments passed to or from other methods.
 #'
 #' @export plot.gears
 #' @export
 plot.gears <- function(x, col = 1, fcol = 4, ylim = NULL, main = NULL,
-                       xlab = "", ylab = "", type = "l", flty = 1, flwd = 2,
-                       ...){
+                       sub = NULL, xlab = "", ylab = "", type = "l", flty = 1,
+                       flwd = 2, cex = 1.75, ...){
 
-  # |__ Original Data ==========================================================
+  # > PLOT FUNCTION ####################################################### ----
 
-  tmpNForecasts <- length(x$out_sample_forecasts)
-  tmpBindTS     <- cbind(x$x, x$out_sample_forecasts)
+  fnct_plot <- function(observed, forecasts, lower, upper, level, betas,
+                        main, sub, ...){
 
-  # |__ Data - Observed and Forecasts ==========================================
+    # |__ Original Data ========================================================
 
-  tmpStart <- stats::start(tmpBindTS)
-  tmpFreq  <- stats::frequency(tmpBindTS)
+    tmpForeLen <- length(forecasts)
+    tmpBindTS  <- cbind(observed, forecasts)
 
-  if (tmpNForecasts > 1){
-    tmpEnd   <- stats::tsp(tmpBindTS)[2] + (round(tmpNForecasts / 2) / tmpFreq)
+    # |__ Data - Observed and Forecasts ========================================
 
-    dataTS <- stats::ts(
-      data      = c(
-        x$x, x$out_sample_forecasts, rep(NA, round(tmpNForecasts / 2))
-      ),
-      start     = tmpStart,
-      end       = tmpEnd,
+    tmpStart <- stats::start(tmpBindTS)
+    tmpFreq  <- stats::frequency(tmpBindTS)
+
+    if (tmpForeLen > 1){
+      tmpEnd   <- stats::tsp(tmpBindTS)[2] + (round(tmpForeLen / 2) / tmpFreq)
+
+      dataTS <- stats::ts(
+        data      = c(observed, forecasts, rep(NA, round(tmpForeLen / 2))),
+        start     = tmpStart,
+        end       = tmpEnd,
+        frequency = tmpFreq
+      )
+
+    } else {
+      tmpEnd   <- stats::tsp(tmpBindTS)[2] + (tmpForeLen / tmpFreq)
+
+      dataTS <- stats::ts(
+        data      = c(observed, forecasts, rep(NA, tmpForeLen)),
+        start     = tmpStart,
+        end       = tmpEnd,
+        frequency = tmpFreq
+      )
+    }
+
+    # |__ Data - Gaps ==========================================================
+
+    tmpStartGAP <- stats::tsp(observed)[2]
+    tmpEndGAP   <- stats::tsp(forecasts)[1]
+
+    dataGAP <- stats::ts(
+      data      = c(utils::tail(observed, 1), utils::head(forecasts, 1)),
+      start     = tmpStartGAP,
+      end       = tmpEndGAP,
       frequency = tmpFreq
     )
 
-  } else {
-    tmpEnd   <- stats::tsp(tmpBindTS)[2] + (tmpNForecasts / tmpFreq)
+    # |__ Dates - Intervals ====================================================
 
-    dataTS <- stats::ts(
-      data      = c(x$x, x$out_sample_forecasts, rep(NA, tmpNForecasts)),
-      start     = tmpStart,
-      end       = tmpEnd,
-      frequency = tmpFreq
+    tmpDatesPolygon <- stats::time(forecasts)
+
+    # |__ Y-Axis Limits ========================================================
+
+    if (is.null(ylim)) {
+      ylim <- c(
+        min(observed, forecasts, na.rm = TRUE),
+        max(observed, forecasts, na.rm = TRUE)
+      )
+      ylim[1] <- min(ylim[1], lower)
+      ylim[2] <- max(ylim[2], upper)
+    }
+
+    # |__ Title ================================================================
+
+    if (is.null(main)) {
+      main <- paste0(
+        "Forecasts from GEARS with ", level, "% prediction interval"
+      )
+    }
+
+    # |__ Subtitle =============================================================
+
+    if (is.null(sub)) {
+      if (betas == "last") {
+        sub <- paste0(
+          "Obtained using the coefficients for the last random sample."
+        )
+      } else {
+        sub <- paste0(
+          "Obtained using the average of coefficients for all random samples."
+        )
+      }
+    }
+
+    # |__ Plot =================================================================
+
+    graphics::plot(
+      x    = dataTS,
+      ylim = ylim,
+      main = main,
+      sub  = sub,
+      col  = col,
+      type = type,
+      xlab = xlab,
+      ylab = ylab
     )
+    graphics::lines(dataGAP, col="white")
+
+    if (tmpForeLen > 1){
+
+      tmpPaddingX <- rep(utils::tail(tmpDatesPolygon, 1) + (0.1 / tmpFreq), 2)
+      tmpPaddingY <- c(utils::tail(lower, 1), utils::head(rev(upper), 1))
+
+      graphics::polygon(
+        x      = c(tmpDatesPolygon, tmpPaddingX, rev(tmpDatesPolygon)),
+        y      = c(lower, tmpPaddingY, rev(upper)),
+        col    = 'grey80',
+        border = NA
+      )
+      graphics::lines(forecasts, col = fcol, lty = flty, lwd = flwd)
+    } else {
+      lowerDate <- c(tmpDatesPolygon) - (0.5 / tmpFreq)
+      upperDate <- c(tmpDatesPolygon) + (0.5 / tmpFreq)
+      graphics::polygon(
+        x      = c(lowerDate, upperDate, upperDate, lowerDate),
+        y      = c(rep(lower, 2), rep(rev(upper), 2)),
+        col    = 'grey80',
+        border = NA
+      )
+      graphics::points(forecasts, col = fcol, lty = flty, lwd = flwd)
+    }
+
   }
 
-  # |__ Data - Gaps ============================================================
+  # > CHECK BETAS ######################################################### ----
 
-  tmpStartGAP <- stats::tsp(x$x)[2]
-  tmpEndGAP   <- stats::tsp(x$out_sample_forecasts)[1]
+  # |__ Each Selection =========================================================
 
-  dataGAP <- stats::ts(
-    data      = c(utils::tail(x$x, 1), utils::head(x$out_sample_forecasts, 1)),
-    start     = tmpStartGAP,
-    end       = tmpEndGAP,
-    frequency = tmpFreq
-  )
-
-  # |__ Dates - Intervals ======================================================
-
-  tmpDatesPolygon <- stats::time(x$out_sample_forecasts)
-
-  # |__ Y-Axis Limits ==========================================================
-
-  if (is.null(ylim)) {
-    ylim <- c(
-      min(x$x, x$out_sample_forecasts, na.rm = TRUE),
-      max(x$x, x$out_sample_forecasts, na.rm = TRUE)
-    )
-
-    ylim[1] <- min(ylim[1], x$lower)
-    ylim[2] <- max(ylim[2], x$upper)
-  }
-
-  # |__ Title ==================================================================
-
-  if (is.null(main)) {
-    main <- paste0(
-      "Forecasts from GEARS with ", x$level, "% prediction interval"
+  if (x$betas != "both") {
+    fnct_plot(
+      observed  = x$x,
+      forecasts = x$out_sample_forecasts,
+      lower     = x$lower,
+      upper     = x$upper,
+      level     = x$level,
+      betas     = x$betas,
+      main      = main,
+      sub       = sub,
+      ...
     )
   }
+  # |__ Both ===================================================================
+  else {
 
-  # |__ Plot ===================================================================
+    # \____ Current par settings -----------------------------------------------
 
-  graphics::plot(
-    x    = dataTS,
-    ylim = ylim,
-    main = main,
-    col  = col,
-    type = type,
-    xlab = xlab,
-    ylab = ylab
-  )
-  graphics::lines(dataGAP, col="white")
+    ## Save current par settings and return after finished
 
-  if (tmpNForecasts > 1){
-    graphics::polygon(
-      x      = c(tmpDatesPolygon, rev(tmpDatesPolygon)),
-      y      = c(x$lower, rev(x$upper)),
-      col    = 'grey80',
-      border = NA
+    op <- par(no.readonly = TRUE)
+    on.exit(par(op))
+
+    # \____ Set new layout -----------------------------------------------------
+
+    layout(matrix(1:length(x$out_sample_forecasts), ncol = 1, byrow = TRUE))
+
+    # \____ Set new par --------------------------------------------------------
+
+    ## Margins
+
+    par(mar = c(3, 3, 2, 3), oma = c(3, 3, 3, 3))
+
+    # \____ Title --------------------------------------------------------------
+
+    ## Main ("Outer")
+
+    if (is.null(main)) {
+      tmpMain <-
+        paste0("Forecasts from GEARS with ", x$level, "% prediction interval")
+    } else {
+      tmpMain <- main
+    }
+
+    ## "Sub-titles"
+
+    tmpMainEach <- list(
+      "Obtained using the coefficients for the last random sample.",
+      "Obtained using the average of coefficients for all random samples."
     )
-    graphics::lines(x$out_sample_forecasts, col = fcol, lty = flty, lwd = flwd)
-  } else {
-    tmpLowerDate <- c(tmpDatesPolygon) - (0.5 / tmpFreq)
-    tmpUpperDate <- c(tmpDatesPolygon) + (0.5 / tmpFreq)
-    graphics::polygon(
-      x      = c(tmpLowerDate, tmpUpperDate, tmpUpperDate, tmpLowerDate),
-      y      = c(rep(x$lower, 2), rep(rev(x$upper), 2)),
-      col    = 'grey80',
-      border = NA
+
+    # \____ Plots --------------------------------------------------------------
+
+    tmpBetas <- list("last", "average")
+
+    tmpPlot <- lapply(
+      X = 1:length(x$out_sample_forecasts),
+      function(X) {
+
+        fnct_plot(
+          observed  = x$x,
+          forecasts = x$out_sample_forecasts[[X]],
+          lower     = x$lower[[X]],
+          upper     = x$upper[[X]],
+          level     = x$level,
+          betas     = tmpBetas[[X]],
+          main      = tmpMainEach[[X]],
+          sub       = "",
+          ...
+        )
+
+        if (X == 1) mtext(tmpMain, outer = T, line = 1, side = 3, cex = cex)
+
+      }
     )
-    graphics::points(x$out_sample_forecasts, col = fcol, lty = flty, lwd = flwd)
+
   }
-
 }
